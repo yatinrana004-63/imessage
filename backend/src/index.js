cb56ecf3de1ccf3e868f190049b14fa27a754c56
Yatin Rana <yatinrana004@gmail.com>
2026-08-18 13:35:58 +0530

    Add cron job to send health check requests every 14 minutes

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ import {clerkMiddleware} from "@clerk/express";
 import cors from "cors";
 import fs from "fs";
 import path from "path";
-// import job from "./lib/cron.js";
+import job from "./lib/cron.js";
 // import clerkWebhook from "./webhooks/clerk.webhook.js";
 const app = express();
 import { fileURLToPath } from "url";
@@ -52,8 +52,8 @@ if (fs.existsSync(publicDir)) {
 app.listen(PORT, () => {
   connectDb();
   console.log(`Server is running on http://localhost:${PORT}`);
-  // job.start()
-  // if(process.env.NODE_ENV === "production"){
-  //   job.start();
-  // }
+  job.start()
+  if(process.env.NODE_ENV === "production"){
+    job.start();
+  }
 });
