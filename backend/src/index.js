b85c7548420ad30bb54e9a53e608f10aa135e517
Yatin Rana <yatinrana004@gmail.com>
2026-08-18 13:17:52 +0530

    Refactor index.js: reorganize imports, enhance static file serving, and improve health check response

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,33 +1,59 @@
-// const express = require("express")
+// const express = require('express');
 import express from "express";
 import "dotenv/config";
-import fs from "fs";
-import path from"path";
+import User from "./models/User.model.js";
+import { connectDb } from "./lib/db.js";
+import {clerkMiddleware} from "@clerk/express";
 import cors from "cors";
-import User from"./models/user.models.js";
-import{ connectDb} from "./lib/db.js";
-import {clerkMiddleware} from '@clerk/express';
+import fs from "fs";
+import path from "path";
+import job from "./lib/cron.js";
+import clerkWebhook from "./webhooks/clerk.webhook.js";
 const app = express();
+import { fileURLToPath } from "url";
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
-const publicDir = path.join(process.cwd(),"public")
-app.use(express.json());
-app.use(cors({ origin : FRONTEND_URL, credentials:true}));
+const publicDir = path.join(process.cwd(), 'public');
+// import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const frontendPath = path.join(__dirname, "../../frontend/dist");
+
+app.use(express.static(frontendPath));
+
+app.get("/*splat", (req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
+});
+
+
+
+app.use("/api/webhooks/clerk", express.raw({ type: "application/json" }),clerkWebhook);
+
+app.use(express.json())
+app.use(cors({origin:FRONTEND_URL, credentials:true}));
 app.use(clerkMiddleware());
-app.get("/health",(req,res)=>{
-    res.status(200).json({ok:true});
-})
 
-if(fs.existsSync(publicDir)){
-    app.use(express.static(publicDir));
 
-    app.get("/{*any}",(req,res,next) =>  {
-      res.sendFile(path.join(publicDir,"index.html"),(err) => next(err));
-    });
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "Server is healthy" });
+});
+
+// app.use("/api/ayth",authRoutes);
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+  app.get('/{*any}', (req, res) => {
+    res.sendFile(path.join(publicDir, 'index.html'),(err) => 
+     next(err));
+  });
 }
 
-console.log("DB_URL=", process.env.MONGO_URI);
-app.listen(PORT, ()=>{ 
-    connectDb();
-    console.log("Server is up and running on PORT:", PORT)});
+app.listen(PORT, () => {
+  connectDb();
+  console.log(`Server is running on http://localhost:${PORT}`);
+  // job.start()
+  if(process.env.NODE_ENV === "production"){
+    job.start();
+  }
+});
